5b77896b04172bcfbd3c3b5277514c766099473b
olgadowns <54759053+olgadowns@users.noreply.github.com>
2020-03-04 15:01:10 +1000

    View papers

--- templates/Papers/view.php
+++ templates/Papers/view.php
@@ -1,6 +1,6 @@
 <h1>Papers</h1>
 
-<h2><?= $paper->title ?></h2>
+<h2><a href="<?= $paper->url ?>" target="_blank"><?= $paper->title ?></a></h2>
 
 <?= $paper->abstract ?>
 
@@ -8,8 +8,7 @@
 <br/>
 
 <center>
-  <a href="/papers/keep/<?=$paper->id?>"><button class="button button-outline">Yes Keep</button></a>
-    <a href="/papers/hide/<?=$paper->id?>"><input class="button button-clear" type="submit" value="No, exclude"></a>
+  <a href="/papers/keep/<?=$paper->id?>"><button class="button button-outline">Yes Keep</button></a><a href="/papers/hide/<?=$paper->id?>"><input class="button button-clear" type="submit" value="No, exclude"></a>
 </center>
 
 <br/>
@@ -38,6 +37,47 @@
 
 </div>
 
+
+<br/>
+
+<h4>Notes</h4>
+<form action="/paper/update/<?= $paper->id?>" method="post">
+<textarea id="notes"></textarea>
+
+<div class="row">
+    <div class="column">
+        <label for="bias">Methodology</label>
+        <select id="bias">
+            <option value="Mixed Methods">Mixed Methods</option>
+            <option value="Quantitative">Quantitative</option>
+            <option value="Qualitative">Qualitative</option>
+            <option value="Other">Other</option>
+        </select>
+    </div>
+    <div class="column">
+        <label for="bias">Bias</label>
+        <select id="bias">
+            <option value="0">No Bias, methodology sound</option>
+            <option value="1">Possible bias, be carefull</option>
+            <option value="3">Obvious bias, dont use</option>
+        </select>
+    </div>
+    <div class="column">
+        <label for="rating">Rating</label>
+        <select id="rating">
+            <option value="0">0 - No Value</option>
+            <option value="1">1 - Interesting, not relevant</option>
+            <option value="2">3 - Contains reference or idea I want to use</option>
+            <option value="3">MUST USE</option>
+        </select>
+    </div>
+    <div class="column">
+        <br/>
+        <br/>
+        <button class="button button-outline" type="submit" style="width: 200px">Save</button>
+    </div>
+</div>
+</form>
 
 <br/>
 
